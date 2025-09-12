0705d115fc2ca6fbf9b19796d2c670c969cf7b0c
Ankur Soni <ankursoni2974@gmail.com>
2025-09-12 10:55:17 +0530

    worked on login page

--- airhockeylivegameclient/src/Components/LoginPage.js
+++ airhockeylivegameclient/src/Components/LoginPage.js
@@ -2,24 +2,31 @@ import React from "react";
 
 const LoginPage = () => {
   return (
-    <div className=" flex flex-col items-center w-7/12">
-      <div>
-        <p>NEON PONG</p>
-        <p>Play. Compete. Climb the leaderboard</p>
+    <div className=" flex flex-col items-center w-7/12 gap-3 bg-black">
+      <div className="flex flex-col items-center gap-3">
+        <div className="flex flex-row items-center gap-1">
+          <span className="text-pink-400">NEON</span>
+          <span className="text-[#21c9ff]">PONG</span>
+        </div>
+        <p className="text-[#b0b6f5]">Play. Compete. Climb the leaderboard</p>
       </div>
-      <div>
-        <form>
+      <div className=" border-s-violet-500 rounded-md border-3 text-white">
+        <form className="flex flex-col items-center gap-1">
           <p>Login</p>
           <div>
-            <label>Email</label>
-            <input type="text"></input>
+            <div className=" flex flex-col">
+              <label>Email</label>
+              <input type="text"></input>
+            </div>
+            <div className=" flex flex-col">
+              <label>Password</label>
+              <input type="text"></input>
+            </div>
           </div>
-          <div>
-            <label>Password</label>
-            <input type="text"></input>
+          <div className="flex flex-col ml-12 ">
+            <p>Forgot password?</p>
           </div>
-          <p>Forgot password?</p>
-          <div>
+          <div className="flex flex-col items-center gap-1">
             <button>Log In</button>
             <p>Don't have an account? Register</p>
           </div>
