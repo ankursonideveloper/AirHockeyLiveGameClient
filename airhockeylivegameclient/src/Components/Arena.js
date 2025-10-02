bd6ef2b56d0f53a20ce62ef87b110e1933a607a3
Ankur Soni <ankursoni2974@gmail.com>
2025-10-02 18:19:01 +0530

    Added player1 paddle

--- airhockeylivegameclient/src/Components/Arena.js
+++ airhockeylivegameclient/src/Components/Arena.js
@@ -1,10 +1,39 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Player1Paddle from "./Player1Paddle";
 
 const Arena = () => {
+  const [position, setPosition] = useState({ x: 0 });
+  const arenaRef = useRef(null);
+
+  useEffect(() => {
+    if (arenaRef) {
+      setPosition({ x: (arenaRef.current.offsetWidth * 5) / 12 });
+    }
+  }, []);
+
+  useEffect(() => {
+    const handleKeyDown = (e) => {
+      switch (e.key) {
+        case "ArrowLeft":
+          setPosition((prev) => ({ ...prev, x: prev.x - 10 }));
+          break;
+        case "ArrowRight":
+          setPosition((prev) => ({ ...prev, x: prev.x + 10 }));
+          break;
+        default:
+          break;
+      }
+    };
+    window.addEventListener("keydown", handleKeyDown);
+    return () => window.removeEventListener("keydown", handleKeyDown);
+  }, []);
+
   return (
-    <div className="w-11/12 sm:w-1/2 mt-1 rounded-md border-2 border-red-600 mx-auto h-screen flex-grow flex flex-col relative">
-      <Player1Paddle />
+    <div
+      className="w-11/12 sm:w-1/2 mt-1 rounded-md border-2 border-red-600 mx-auto h-screen flex-grow flex flex-col relative"
+      ref={arenaRef}
+    >
+      <Player1Paddle position={position} />
     </div>
   );
 };
--- airhockeylivegameclient/src/Components/Player1Paddle.js
+++ airhockeylivegameclient/src/Components/Player1Paddle.js
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Player1Paddle = () => {
+const Player1Paddle = ({ position }) => {
+  const [currentPosition, setCurrentPosition] = useState({ x: position });
+
   return (
-    <div className="w-1/6 h-10 bg-blue-500 rounded-md absolute bottom-0 left-1/2 -translate-x-1/2"></div>
+    <div
+      className="w-1/6 h-10 bg-blue-500 rounded-md absolute bottom-0 "
+      tabIndex={0}
+      style={{ left: `${currentPosition.x}px` }}
+    ></div>
   );
 };
 
