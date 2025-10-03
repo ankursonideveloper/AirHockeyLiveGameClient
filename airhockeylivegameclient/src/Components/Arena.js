6bee38b272236b19514101d4f09ce4fd1508fc51
Ankur Soni <ankursoni2974@gmail.com>
2025-10-03 11:31:14 +0530

    working on resizing logic of paddle

--- airhockeylivegameclient/src/Components/Arena.js
+++ airhockeylivegameclient/src/Components/Arena.js
@@ -2,23 +2,42 @@ import React, { useEffect, useRef, useState } from "react";
 import Player1Paddle from "./Player1Paddle";
 
 const Arena = () => {
-  const [position, setPosition] = useState({ x: 0 });
   const arenaRef = useRef(null);
+  const arenaInitialWidthRef = useRef(null);
+  const [position, setPosition] = useState({ x: 0 });
+  const [onLeftMost, setOnLeftMost] = useState(false);
+  const [onRightMost, setOnRightMost] = useState(false);
 
   useEffect(() => {
-    if (arenaRef) {
+    if (arenaRef.current) {
       setPosition({ x: (arenaRef.current.offsetWidth * 5) / 12 });
+      arenaInitialWidthRef.current = arenaRef.current.offsetWidth;
     }
   }, []);
 
   useEffect(() => {
     const handleKeyDown = (e) => {
+      const currentWidthOfArena = arenaRef.current.clientWidth;
       switch (e.key) {
         case "ArrowLeft":
-          setPosition((prev) => ({ ...prev, x: prev.x - 10 }));
+          setPosition((prev) => {
+            if (prev.x <= 1) {
+              return { x: 0 };
+            } else {
+              return { x: prev.x - 2 };
+            }
+          });
           break;
+
         case "ArrowRight":
-          setPosition((prev) => ({ ...prev, x: prev.x + 10 }));
+          setPosition((prev) => {
+            if (prev.x >= (currentWidthOfArena * 5) / 6 - 2) {
+              return { x: (currentWidthOfArena * 5) / 6 };
+            } else {
+              return { x: prev.x + 2 };
+            }
+          });
+
           break;
         default:
           break;
@@ -26,6 +45,41 @@ const Arena = () => {
     };
     window.addEventListener("keydown", handleKeyDown);
     return () => window.removeEventListener("keydown", handleKeyDown);
+  }, []);
+
+  useEffect(() => {
+    const handleResize = () => {
+      let arenaNewWidth = arenaRef.current.offsetWidth;
+      let arenaLastWidth = arenaInitialWidthRef.current;
+      if (arenaNewWidth > arenaLastWidth) {
+        console.log("Width is increasing → expanding");
+        setPosition((prev) => {
+          if (prev.x == 0) {
+            return { x: prev.x };
+          } else if (prev.x >= (5 * arenaNewWidth) / 12 - 1) {
+            return { x: (5 * arenaNewWidth) / 12 };
+          } else {
+            return { x: prev.x + (5 * (arenaNewWidth - arenaLastWidth)) / 12 };
+          }
+        });
+      } else if (arenaNewWidth < arenaLastWidth) {
+        console.log("Width is decreasing → shrinking");
+        setPosition((prev) => {
+          if (prev.x <= 1) {
+            return { x: 0 };
+          } else if (prev.x == (5 * arenaNewWidth) / 12) {
+            return { x: prev.x };
+          } else {
+            return { x: prev.x - (5 * (arenaLastWidth - arenaNewWidth)) / 12 };
+          }
+        });
+      }
+
+      arenaInitialWidthRef.current = arenaNewWidth;
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
   }, []);
 
   return (
--- airhockeylivegameclient/src/Components/Player1Paddle.js
+++ airhockeylivegameclient/src/Components/Player1Paddle.js
@@ -1,13 +1,10 @@
 import React, { useState } from "react";
 
 const Player1Paddle = ({ position }) => {
-  const [currentPosition, setCurrentPosition] = useState({ x: position });
-
   return (
     <div
-      className="w-1/6 h-10 bg-blue-500 rounded-md absolute bottom-0 "
-      tabIndex={0}
-      style={{ left: `${currentPosition.x}px` }}
+      className="w-1/6 h-10 bg-blue-500 rounded-sm absolute bottom-0 "
+      style={{ left: `${position.x}px` }}
     ></div>
   );
 };
