80507592e77e85e41e934cc746d63a227719230e
Ankur Soni <ankursoni2974@gmail.com>
2025-10-23 12:55:54 +0530

    able to get players in dropdown

--- airhockeylivegameclient/src/Components/Dashboard.js
+++ airhockeylivegameclient/src/Components/Dashboard.js
@@ -6,34 +6,42 @@ import AvailablePlayers from "./AvailablePlayers";
 
 const Dashboard = () => {
   const [onlinePlayers, setOnlinePlayers] = useState([]);
+  const [playState, setPlayState] = useState(false);
 
   useEffect(() => {
-    console.log("✅ useEffect in Dashboard running...");
-    const token = localStorage.getItem("authToken");
+    if (!playState) {
+      console.log("✅ useEffect in Dashboard running...");
+      const token = localStorage.getItem("authToken");
 
-    const socket = io("http://localhost:5000", { auth: { token } });
+      const socket = io("http://localhost:5000", { auth: { token } });
 
-    socket.on("connect", () => {
-      console.log("✅ Socket connected, ID:", socket.id);
-      socket.emit("messageFromClient", {
-        msg: "Hello from React1" + socket.id,
+      socket.on("connect", () => {
+        console.log("✅ Socket connected, ID:", socket.id);
+        socket.emit("messageFromClient", {
+          msg: "Hello from React1" + socket.id,
+        });
       });
-    });
 
-    socket.on("connect_error", (err) => {
-      console.error("❌ Socket connection error:", err);
-    });
+      socket.on("availableUsersUpdate", (data) => {
+        console.log("Received players:", data);
+        setOnlinePlayers(data);
+      });
 
-    return () => {
-      console.log("❌ Disconnecting socket...");
-      socket.disconnect();
-    };
+      socket.on("connect_error", (err) => {
+        console.error("❌ Socket connection error:", err);
+      });
+
+      return () => {
+        console.log("❌ Disconnecting socket...");
+        socket.disconnect();
+      };
+    }
   }, []);
 
   return (
     <div className="text-slate-300 w-full flex flex-col h-screen mb-1">
       <Navigation />
-      <AvailablePlayers availablePlayers={["Ankur", "Akash"]} />
+      <AvailablePlayers availablePlayers={onlinePlayers} />
       <Arena />
     </div>
   );
